07929ef88e5d9efd615314d774fec6fe1dda82ea
cchalkias <cch@cchnet.gr>
2021-11-16 17:31:29 +0200

    Update SkillInfo.cs

--- SkillInfo.cs
+++ SkillInfo.cs
@@ -53,7 +53,7 @@ namespace CoPilot
         internal static Skill bladeVortex = new Skill();
         internal static Skill plagueBearer = new Skill();
         internal static Skill bladeBlast = new Skill();
-        internal static Skill holyRelict = new Skill();
+        internal static Skill holyRelic = new Skill();
         internal static Skill berserk = new Skill();
         internal static Skill sweep = new Skill();
         internal static Skill witherStep = new Skill();
@@ -97,7 +97,7 @@ namespace CoPilot
             bladeVortex = new Skill();
             plagueBearer = new Skill();
             bladeBlast = new Skill();
-            holyRelict = new Skill();
+            holyRelic = new Skill();
             berserk = new Skill();
             sweep = new Skill();
             witherStep = new Skill();
@@ -284,7 +284,7 @@ namespace CoPilot
                         bladeBlast.Id = skill.Id;
                         break;
                     case "summon_relic":
-                        holyRelict.Id = skill.Id;
+                        holyRelic.Id = skill.Id;
                         break;
                     case "berserk":
                         berserk.Id = skill.Id;
